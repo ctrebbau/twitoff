7e908eaeddea7f129717d19a707a1c3658c21e0e
Carlos-Trebbau <ctrebbau@pm.me>
2021-10-16 19:52:19 +0200

    DB reset via route

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.twitter_routes import twitter_routes
+from web_app.routes.admin_routes import admin_routes
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'development_db.db')
@@ -23,6 +24,7 @@ def create_app():
 
     app.register_blueprint(home_routes)
     app.register_blueprint(twitter_routes)
+    app.register_blueprint(admin_routes)
 
     return app
 
